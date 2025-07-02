1382ed5c5b3ab949e7abac286f70a9ae23990cc0
Enkata DevOps <gakrchintugmailcom@example.com>
2025-07-02 17:43:38 +0000

    Add login page to app.py

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "<h1>Deployed using Jenkins + Ansible!</h1>"
+    return """
+    <h1>Login Page</h1>
+    <form>
+        <input placeholder='Username'><br>
+        <input type='password' placeholder='Password'><br>
+        <button>Login</button>
+    </form>
+    """
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run()
+
